7bbee24d211131b2e3ef96d0e8c9bc4cf84e71d7
Jean <jean@data.gov.sg>
2018-04-05 20:06:26 +0800

    Add call to AWS

--- ApexCSharp/MCTest.cs
+++ ApexCSharp/MCTest.cs
@@ -13,16 +13,18 @@ namespace ApexCSharp
         const string _proxyTargetHost = "";
         const string _sourceSigningHost = "";
         const string _sourceTargetHost = "";
+        const string _awsHost = "";
         private static System.Collections.Specialized.NameValueCollection _settings = ConfigurationManager.AppSettings;
         private static int _totalAsserts = 0;
         private static int _failedAsserts = 0;
         public static void Main(string[] args){
                
-                SingleTest();
+                PipedTests();
+                Test_Can_SMS();
                 Console.WriteLine($"_totalAsserts: { _totalAsserts} _failedAsserts: {_failedAsserts}");
         }
 
-        static void SingleTest(){
+        static void PipedTests(){
                 Test_Is_MC_Working();
                 var content = Test_Can_Create_MC()?.Content.ReadAsStringAsync().Result;
                 if (content != null){
@@ -68,6 +70,18 @@ namespace ApexCSharp
             CheckEquals(response?.StatusCode , HttpStatusCode.OK);
             return response;
         }
+        static System.Net.Http.HttpResponseMessage Test_Can_SMS(){
+            var endpoint = "/sms";
+            var proxy = new Gateway (_settings, GatewayType.PROXY, "L1",
+                    signingUrlPath: $"{_awsHost}{endpoint}", 
+                    targetUrlPath: $"{_awsHost}{endpoint}");
+            var helper = new MCHelper(GatewayType.PROXY, proxy, null, ApiUtilLib.HttpMethod.POST);
+            var body = GenerateSMSBody();
+            var response = helper.Send(body).GetAwaiter().GetResult();
+            CheckEquals(response?.StatusCode , HttpStatusCode.OK);
+            return response;
+        }
+
 
          static MCHelper GetMCHelper (string endpoint, ApiUtilLib.HttpMethod httpMethod){
             var proxy = new Gateway (_settings, GatewayType.PROXY, "L2",
@@ -80,9 +94,7 @@ namespace ApexCSharp
         }
 
         static string GenerateCreateBody(string id = null){
-            if(id == null){
-                id = Guid.NewGuid().ToString();
-            }
+            id = id ?? Guid.NewGuid().ToString();
             var time = System.DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ssK");
             return "{\"CreatedByName\": \"MCTest\", \"CreatedWhen\": \""+time+"\", \"ProviderMCR\": \"string\", \"PatientID\": \"string\", \"PatientName\": \"string\", \"PhoneNumber\": \"string\", \"Institution\": \"Singapore General Hospital\", \"VisitID\": \""+id+"\", \"AdmitDatetime\": \"\", \"DischargeDatetime\": \"\", \"Department\": \"string\", \"Ward\": \"string\", \"Status\": \"Active\", \"MedicalCertificateID\": \""+id+"\", \"FlagUnfit\": true, \"FlagFit\": true, \"FlagCourt\": true, \"FlagDuration\": true, \"MCType\": 0, \"SignificantDtm\": \"\", \"UnfitFrom\": \"\", \"UnfitTo\": \"\", \"FitFrom\": \"\", \"FitTo\": \"\", \"DurationFrom\": \"\", \"DurationTo\": \"\", \"DutyComment\": \"\", \"Diagnosis\": \"\", \"SurgicalOperations\": \"\", \"VoidBy\": \"\", \"VoidWhen\": \"\", \"KKHDeliveryDate\": \"\"}";
             
@@ -90,6 +102,12 @@ namespace ApexCSharp
         static string GenerateVoidBody(string name = null){
             var time = System.DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ssK");
             return "{ \"VoidBy\": \"MCTest\", \"VoidWhen\": \""+time+"\"}";
+        }
+
+        static string GenerateSMSBody(string id = null, string phoneNumber=null){
+            id = id ?? Guid.NewGuid().ToString();
+            phoneNumber = phoneNumber ?? "00000000";
+            return "{\"id\": \""+id+"\", \"phoneNumber\": \""+phoneNumber+"\"}";
         }
 
         static bool CheckEquals(object a, object b, [System.Runtime.CompilerServices.CallerMemberName] string callerMember= ""){
